960a438ff1b3f4c374ed21a8e93b1ce779c59c3b
Admin <admin@Mac-Admin.local>
2021-01-15 20:18:02 +0300

    stable | almost normed

--- srcs/parser_quote_util.c
+++ srcs/parser_quote_util.c
@@ -12,95 +12,96 @@
 
 #include "../includes/minishell.h"
 
-static char *ft_crt_res(char *line, int start, int *i, int *flag)
+static char	*ft_crt_res(char *line, int start, int *i, int *flag)
 {
-    char *res;
+	char	*res;
 
-    *flag = 0;
-    res = ft_substr(line, start, *i - start);
-    (*i)++;
-    return (res);
+	*flag = 0;
+	res = ft_substr(line, start, *i - start);
+	(*i)++;
+	return (res);
 }
 
-char	*parse_squote(char *arg, char *line, int *i)
+char		*parse_squote(char *arg, char *line, int *i)
 {
-    char    *res;
-    int     flag;
-    int     start;
+	char	*res;
+	int		flag;
+	int		start;
 
-    flag = 1;
-    (*i)++;
-    start = *i;
-    while (line[(*i)] && flag)
-    {
-        if (line[(*i)] == '\'')
-        {
-            res = ft_crt_res(line, start, i, &flag);
+	flag = 1;
+	(*i)++;
+	start = *i;
+	while (line[(*i)] && flag)
+	{
+		if (line[(*i)] == '\'')
+		{
+			res = ft_crt_res(line, start, i, &flag);
 			arg = ft_strjoin(arg, res); //need add  free() in strjoin
 			free(res);
-            return (arg);
-        }
-        (*i)++;
-    }
-    if (flag)
-   {
-        ft_putstr_fd("error \'\n", 1);
-        arg = ft_strdup("");
-        return (arg);
-    }
+			return (arg);
+		}
+		(*i)++;
+	}
+	if (flag)
+	{
+		ft_putstr_fd("error \'\n", 1);
+		arg = ft_strdup("");
+		return (arg);
+	}
 	return (NULL);
 }
 
-char    *simple_parse(char *arg, char *line, int *i, t_data *data)
+char		*simple_parse(char *arg, char *line, int *i, t_data *data)
 {
-    char    *envp_value = NULL;
+	char	*envp_value;
 
-    while (line[(*i)] && (!ft_strchr(" ;><|\'\"", line[(*i)])))
-    {
+	envp_value = NULL;
+	while (line[(*i)] && (!ft_strchr(" ;><|\'\"", line[(*i)])))
+	{
 		if (line[(*i)] == '\\')
 			(*i)++;
 		else if (line[(*i)] == '$')
-            envp_value = parse_envp(data, line, i);
-        if (envp_value)
-            arg = ft_strjoin(arg, envp_value);
-        else
-            arg = add_symbol(arg, line[(*i)]);
-        (*i)++;
-    }
-    return(arg);
+			envp_value = parse_envp(data, line, i);
+		if (envp_value)
+			arg = ft_strjoin(arg, envp_value);
+		else
+			arg = add_symbol(arg, line[(*i)]);
+		(*i)++;
+	}
+	return (arg);
 }
 
-static int     check_dq(char c)
+static int	check_dq(char c)
 {
-    return (c == '"');
+	return (c == '"');
 }
 
-char    *parse_dquote(char *arg, char *line, int *i, t_data *data)
+char		*parse_dquote(char *arg, char *line, int *i, t_data *data)
 {
-    int     flag;
+	int		flag;
 
-    flag = check_dq(line[++(*i)]);
-    while (line[(*i)] && flag == 0)
-    {
-        if ((flag = check_dq(line[(*i)])))
-            break;
+	flag = check_dq(line[++(*i)]);
+	while (line[(*i)] && flag == 0)
+	{
+		if ((flag = check_dq(line[(*i)])))
+			break ;
 		if (line[(*i)] == '\\' && (ft_strchr("$\"\\", line[(*i) + 1])))
-        {
+		{
 			(*i)++;
-            arg = add_symbol(arg, line[(*i)++]);
-            if ((flag = check_dq(line[(*i)])))
-                break;
-        }
+			arg = add_symbol(arg, line[(*i)++]);
+			if ((flag = check_dq(line[(*i)])))
+				break ;
+		}
 		if (line[(*i)] == '$')
-            arg = ft_strjoin(arg, parse_envp(data, line, i)); // free
-        else
-            arg = add_symbol(arg, line[(*i)++]);
-    }
-    (*i)++;
-    if (!flag)
-    {
-        ft_putstr_fd("error \"\n", 1);
-        arg = ft_strdup("");
-    }
+			arg = ft_strjoin(arg, parse_envp(data, line, i)); // free
+		else
+			arg = add_symbol(arg, line[(*i)++]);
+	}
+	(*i)++;
+	if (!flag)
+	{
+		ft_putstr_fd("error \"\n", 1);
+		arg = ft_strdup("");
+	}
 	return (arg);
 }
--- srcs/parser_utils.c
+++ srcs/parser_utils.c
@@ -12,25 +12,25 @@
 
 #include "../includes/minishell.h"
 
-char    *add_symbol(char *str, char c)
+char		*add_symbol(char *str, char c)
 {
-    char    *res;
-    int     i;
+	char	*res;
+	int		i;
 
-    i = 0;
-    res = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2)); // if
-    while (str[i])
-    {
-        res[i] = str[i];
-        i++;
-    }
-    res[i++] = c;
-    res[i] = '\0';
-    free(str);
-    return (res);
+	i = 0;
+	res = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2)); // if
+	while (str[i])
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i++] = c;
+	res[i] = '\0';
+	free(str);
+	return (res);
 }
 
-char	**double_array_realloc(char **array, int size)
+char		**double_array_realloc(char **array, int size)
 {
 	int		i;
 	char	**tmp;
@@ -57,47 +57,46 @@ char	**double_array_realloc(char **array, int size)
 	return (array);
 }
 
-void    ft_check_list(t_args *tab)
+void		ft_check_list(t_args *tab)
 {
-    int     *save_sym;
-    int     i;
-    t_args  *tmp;
+	int		*save_sym;
+	int		i;
+	t_args	*tmp;
 
-    if (!tab)
-        return ;
-    i = 0;
-    if (tab->next == NULL)
-        tab->simbol = 0;
-    else
-    {
-        tab->simbol_last = 0;
-        tmp = tab;
-        save_sym = (int *)malloc(sizeof(int) * ft_list_len(tab)); // error malloc
-        while (tmp)
-        {
-            save_sym[i] = tmp->simbol;
-            tmp = tmp->next;
-            i++;
-        }
-        ft_set_simbol(tab, save_sym);
-        free(save_sym);
-    }
+	if (!tab)
+		return ;
+	i = 0;
+	if (tab->next == NULL)
+		tab->simbol = 0;
+	else
+	{
+		tab->simbol_last = 0;
+		tmp = tab;
+		save_sym = (int *)malloc(sizeof(int) * ft_list_len(tab)); // error malloc
+		while (tmp)
+		{
+			save_sym[i] = tmp->simbol;
+			tmp = tmp->next;
+			i++;
+		}
+		ft_set_simbol(tab, save_sym);
+		free(save_sym);
+	}
 }
 
-char    **ft_crt_arr(char **arr, char *str, int *i)
+char		**ft_crt_arr(char **arr, char *str, int *i)
 {
-
-    arr = double_array_realloc(arr, 1);
-    arr[(*i)] = ft_strdup(str);
-    (*i)++;
-    free(str);
-    str = NULL;
-    return (arr);
+	arr = double_array_realloc(arr, 1);
+	arr[(*i)] = ft_strdup(str);
+	(*i)++;
+	free(str);
+	str = NULL;
+	return (arr);
 }
 
-int    ft_skip_space(char *str, int i)
+int			ft_skip_space(char *str, int i)
 {
-    while (str[i] == ' ')
-        i++;
-    return (i);
+	while (str[i] == ' ')
+		i++;
+	return (i);
 }
